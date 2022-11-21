a43241e28e5721ec5185784ace027f210ec9ee4b
Olesia Gorning <olesja.a92@gmail.com>
2022-11-21 17:03:36 +0300

    задача meetuptitle

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,17 +1,37 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
-function fetchMeetupById(meetupId) {
-  return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      return response.json().then((error) => {
-        throw error;
-      });
-    }
-  });
+const App = {
+	data() {
+		return {
+			meetupId: 0,
+			activeMeetup: '',
+		};
+	},
+	watch: {
+		meetupId(val) {
+			var _this = this;
+			this.fetchMeetupById(val).then((meetup) => {
+				_this.activeMeetup = meetup; //3
+			  });
+		},
+	},
+	methods: {
+		fetchMeetupById(meetupId) {
+		  return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
+			if (response.ok) {
+			  return response.json();
+			} else {
+			  return response.json().then((error) => {
+				throw error;
+			  });
+			}
+		  });
+		}
+	}
 }
+
+createApp(App).mount('#app');
 
 // Требуется создать Vue приложение
